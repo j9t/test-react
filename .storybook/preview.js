6500adca9aa5435f8fd8d7b358e712a5288a3165
Jens Oliver Meiert <jens@meiert.com>
2024-11-07 22:38:29 +0100

    Updates

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/index.css';
+
+export const parameters = {
+  actions: {
+    handles: ['onClick', 'onSubmit', 'onChange'], // Explicitly define actions
+  },
+};
